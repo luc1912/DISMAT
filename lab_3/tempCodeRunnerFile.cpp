f079405a27ea23349664642fc0c1fcaf5d327f53
Lucija Runjić <97415911+luc1912@users.noreply.github.com>
2023-05-21 17:18:43 +0200

    Delete tempCodeRunnerFile.cpp

--- lab_3/tempCodeRunnerFile.cpp
+++
@@ -1,7 +0,0 @@
-  // for (int i = 0; i < n; i++) {
-    //     for (int j = 0; j < n; j++) {
-    //         cout << *(mat_sus + n * i + j) << " ";
-    //     }
-    //     cout << endl;
-    // }
-    // cout << endl;
